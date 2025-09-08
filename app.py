3c31fc67c0bc8a4f0d0888c514ea51457a5ab203
Salomão Machaieie <135821028+MachaieieSalo@users.noreply.github.com>
2025-09-08 02:56:34 +0200

    app.py

--- app.py
+++ app.py
@@ -234,10 +234,7 @@ if new_photo:
 # ==========================
 
 # Centralizar logo usando colunas
-col1, col2, col3 = st.columns([1, 2, 1])
-with col2:
-    st.image("static/image/logo.png", width=120, output_format="PNG", caption="")  # caminho relativo
-st.title("📚 Biblioteca Virtual | Instituto Politécnico Sumayya")
+
 
 # Pesquisa
 search = st.text_input("🔍 Procurar livro por título")
@@ -366,6 +363,7 @@ if user_email == ADMIN_EMAIL:
 
         except Exception as e:
             st.error(f"Ocorreu um erro ao gerar DOCX: {e}")
+
 
 
 
